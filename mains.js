cd43a7041698f6592622d7288d3f52d6c6f934af
ZhengHk <zhenghk@yahoo.de>
2020-09-08 16:24:24 +0200

    Marker and zoom functionality added

--- mains.js
+++ mains.js
@@ -1,7 +1,7 @@
 window.onload = init;
 
 function init(){
-    var map = new ol.Map({
+    let map = new ol.Map({
     target: 'map',
     layers: [
       new ol.layer.Tile({
@@ -14,24 +14,77 @@ function init(){
     })
   });
 
-  map.on('click', function(e){
-      console.log(e.coordinate);
-      console.log(ol.proj.toLonLat(e.coordinate));
-      
-  })
   
-  var geocoder = new maptiler.Geocoder({
+  console.log('test');
+  let geocoder = new maptiler.Geocoder({
     input: 'search',
     key: 'BqKwpNnya8hizpvG7OlA'
   });
   geocoder.on('select', function(item) {
     console.log('Selected', item);
     map.getView().setCenter(ol.proj.fromLonLat(item.center));
+    console.log(item.bbox);
+
+    let tempArray = item.bbox;
+    tempArray[4] = item.bbox[0];
+    let coordinates = 
+      [
+        [
+          ol.proj.fromLonLat([item.bbox[0],
+          item.bbox[1]])
+        ],
+        [
+          ol.proj.fromLonLat([item.bbox[2],
+            item.bbox[1]])
+        ],
+        [
+          ol.proj.fromLonLat([item.bbox[2],
+            item.bbox[3]])
+        ],
+        [
+          ol.proj.fromLonLat([item.bbox[0],
+            item.bbox[3]])
+        ],
+        [
+          ol.proj.fromLonLat([item.bbox[0],
+            item.bbox[1]])
+        ]
+      ]
+    let poly = new ol.geom.Polygon(coordinates);
+    console.log(poly);
+    map.getView().fit(poly);
+    
+    let layer = new ol.layer.Vector({
+      source: new ol.source.Vector({
+          features: [
+              new ol.Feature({
+                  geometry: new ol.geom.Point(ol.proj.fromLonLat(item.center))
+              })
+          ]
+      }),
+      style: new ol.style.Style({
+        image: new ol.style.Icon({
+          anchor: [0.5, 1],
+          src: './data/174-free-google-maps-pointer.png',
+          scale: 0.1,
+        }),
+      }),
   });
+  
+  map.on('click', function(e){
+    console.log(e.coordinate);
+    console.log(ol.proj.toLonLat(e.coordinate));
+    map.removeLayer(layer);
+})
+
+ 
+  map.addLayer(layer);
+  });
+  console.log('After');
 
 }
 
-  
+
       
 
 
